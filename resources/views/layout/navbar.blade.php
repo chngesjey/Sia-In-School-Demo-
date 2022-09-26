589f7b65eb9f11c21194bab3693b812f7699f16f
chngesjey <nandoaja901@gmail.com>
2022-09-26 07:50:57 +0700

    Sytling

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,23 +1,24 @@
-<nav class="navbar navbar-expand-lg bg-info">
+<nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Sistem Sekolah</a>
+  <div class="text-bg-secondary p-3"><i class="fa-sharp fa-solid fa-school"></i><a class="navbar-brand text-light" href="/">Sistem Sekolah</a></div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
 
         <li class="nav-item">
-          <a class="nav-link" href="/siswa">Siswa</a>
+        <a class="nav-link" href="/siswa"><div class="text-light">Siswa</div></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/guru">Guru</a>
+          <a class="nav-link" href="/guru"><div class="text-light">Guru</div></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/kelas">Kelas</a>
+          <a class="nav-link" href="/kelas"><div class="text-light">Kelas</div></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/mapel">Mapel</a>
+          <a class="nav-link" href="/mapel"><div class="text-light">Mapel</div></a>
         </li>
     
       </ul>
